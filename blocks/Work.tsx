c28d61c2d342b777fcaa6bcb5295099d03f21514
nstelmach <stelmach.n@gmail.com>
2023-05-15 12:36:45 +0200

    styles

--- blocks/Work.tsx
+++ blocks/Work.tsx
@@ -37,7 +37,7 @@ export default function Work(props: WorkProps) {
     }) => (
       <div
         key={key}
-        className="flex justify-center items-center flex-col rounded-lg bg-dark-grey shadow-grey max-w-xs sm:max-w-md p-8"
+        className="flex justify-center items-center flex-col rounded-lg bg-dark-grey shadow-grey max-w-md sm:max-w-xl lg:max-w-sm xl:max-w-md p-8"
       >
         <h2 className="text-lg mb-8 text-center font-semibold tracking-xl hover:scale-110 drop-shadow-xs hover:transition-all hover:cursor-pointer">
           {title}
@@ -90,7 +90,7 @@ export default function Work(props: WorkProps) {
         <h2 className="text-3xl sm:text-5xl md:text-6xl tracking-xl lg:tracking-xxl drop-shadow-s mb-16 mt-6 sm:mb-20 sm:mt-8 md:mb-24 md:mt-10">
           {title}
         </h2>
-        <div className="flex justify-evenly flex-wrap md:gap-7 gap-10 sm:p-12 p-8 bg-[url('../public/images/white-concrete-wall.jpg')] bg-repeat bg-cover shadow-white sm:max-w-xl max-w-sm md:max-w-2xl lg:max-w-4xl xl:max-w-5xl rounded-lg">
+        <div className="flex justify-evenly flex-wrap md:gap-10 gap-6 sm:p-12 p-5 sm:p-8 bg-[url('../public/images/white-concrete-wall.jpg')] bg-repeat bg-cover shadow-white max-w-lg sm:max-w-xl md:max-w-2xl lg:max-w-4xl xl:max-w-5xl rounded-lg">
           {project}
         </div>
       </div>
